169a4dc7978526326356c0f6f40e20b61a588362
cmkhombo <cmkhombo@gmail.com>
2022-07-27 02:06:59 +0200

    first commit sum of multiples task

---
+++ task1.1.py
@@ -0,0 +1,9 @@
+def sum_of_multiples(_max_num):
+    _sum = 0
+    for _num in range(0, _max_num):
+        if _num % 3 == 0 or _num % 5 == 0:
+            _sum += _num
+    print(_sum)
+
+
+sum_of_multiples(1000)
